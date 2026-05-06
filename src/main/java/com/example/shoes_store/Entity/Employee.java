599f474107a096150e6e45bd79c11191dfc1edee
cuongmai <cuongll912003@gmail.com>
2026-05-06 09:49:10 +0700

    save demodashboard

--- src/main/java/com/example/shoes_store/Entity/Employee.java
+++ src/main/java/com/example/shoes_store/Entity/Employee.java
@@ -2,6 +2,7 @@ package com.example.shoes_store.Entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -11,7 +12,7 @@ import java.time.LocalDate;
 @Entity
 public class Employee {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String code;
     private String fullName;
